e44f8d3522c854c3062699692c57222fe332e273
Joe Lencioni <jlencion@gustavus.edu>
2012-03-06 10:51:49 -0600

    Added test for date()

--- Test/ValidateTest.php
+++ Test/ValidateTest.php
@@ -49,4 +49,50 @@ class ValidateTest extends Test
   {
     $this->assertSame($expected, Validate::email($email));
   }
+
+  /**
+   * @return array
+   */
+  public static function dateData()
+  {
+    return array(
+      array(true, '01/09/1983'),
+      array(true, '01/9/1983'),
+      array(true, '1/09/1983'),
+      array(true, '1/9/1983'),
+      array(true, '01/09/83'),
+      array(true, '1/09/83'),
+      array(true, '01/9/83'),
+      array(true, '1/9/83'),
+
+      array(true, '1983/01/09'),
+      array(true, '1983/01/9'),
+      array(true, '1983/1/09'),
+      array(true, '1983/1/9'),
+
+      array(true, '01-09-1983'),
+      array(true, '1-09-1983'),
+      array(true, '01-9-1983'),
+      array(true, '1-9-1983'),
+      array(true, '01-09-83'),
+      array(true, '1-09-83'),
+      array(true, '01-9-83'),
+      array(true, '1-9-83'),
+
+      array(false, '83/01/09'),
+      array(false, '13/09/1983'),
+      array(false, '01/32/1983'),
+    );
+  }
+
+  /**
+   * @test
+   * @dataProvider dateData
+   * @param boolean $expected
+   * @param string $date
+   */
+  public function date($expected, $date)
+  {
+    $this->assertSame($expected, Validate::date($date));
+  }
 }
